ace5da04a1cee7907ca67f9fcf112b926e8b684a
abdulrahim860 <abdulrahim82436@gmail.com>
2025-03-29 11:38:19 +0530

    code updated

--- gen ai proj.py
+++ gen ai proj.py
@@ -1,3 +1,6 @@
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
@@ -12,3 +15,20 @@ import urllib.request
 ssl._create_default_https_context = ssl._create_unverified_context
 
 (X_train, _), (_, _) = mnist.load_data()
+
+def build_generator():
+    model = Sequential()
+    model.add(Dense(256, input_dim=100))
+    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(512))
+    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(1024))
+    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Dense(784, activation='tanh'))
+    model.add(Reshape((28, 28, 1)))
+    return model
+
+generator = build_generator()
