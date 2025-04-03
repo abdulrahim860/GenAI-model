e2c6a1c1730decdce0ab379a4f5077625192e87e
abdulrahim860 <abdulrahim82436@gmail.com>
2025-04-03 20:03:06 +0530

    Trained GAN model and updated weights

--- gen_ai_gan.py
+++ gen_ai_gan.py
@@ -54,3 +54,29 @@ generated_image=generator(gan_input)
 gan_output=discriminator(generated_image)
 gan=Model(gan_input,gan_output)
 gan.compile(optimizer=Adam(0.0002,0.5),loss='binary_crossentropy')
+
+def train_gan(epochs, batch_size=128):
+    X_train, _ = mnist.load_data()
+    X_train = (X_train[0].astype(np.float32) - 127.5) / 127.5
+    X_train = np.expand_dims(X_train, axis=3)
+    
+    real = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
+    
+    for epoch in range(epochs):
+        idx = np.random.randint(0, X_train.shape[0], batch_size)
+        real_images = X_train[idx]
+        
+        noise = np.random.normal(0, 1, (batch_size, 100))
+        generated_images = generator.predict(noise)
+        
+        d_loss_real = discriminator.train_on_batch(real_images, real)
+        d_loss_fake = discriminator.train_on_batch(generated_images, fake)
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        
+        noise = np.random.normal(0, 1, (batch_size, 100))
+        g_loss = gan.train_on_batch(noise, real)
+        
+        if epoch % 100 == 0:
+            print(f"{epoch} [D loss: {d_loss[0]}, acc.: {100*d_loss[1]}] [G loss: {g_loss}]")
+            save_images(epoch)
