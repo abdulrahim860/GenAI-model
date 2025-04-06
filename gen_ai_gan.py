9af43a63069c32103f378f543fbb2cec6534bf57
abdulrahim860 <abdulrahim82436@gmail.com>
2025-04-06 21:45:17 +0530

    neg slope changed to alpha

--- gen_ai_gan.py
+++ gen_ai_gan.py
@@ -20,13 +20,13 @@ def build_generator():
     model = Sequential()
     model.add(Input(shape=(100,)))  
     model.add(Dense(256))
-    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(LeakyReLU(alpha=0.2))  # Changed here
     model.add(BatchNormalization(momentum=0.8))
     model.add(Dense(512))
-    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(LeakyReLU(alpha=0.2))  # Changed here
     model.add(BatchNormalization(momentum=0.8))
     model.add(Dense(1024))
-    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(LeakyReLU(alpha=0.2))  # Changed here
     model.add(BatchNormalization(momentum=0.8))
     model.add(Dense(784, activation='tanh'))
     model.add(Reshape((28, 28, 1)))
@@ -39,21 +39,21 @@ def build_discriminator():
     model.add(Input(shape=(28, 28, 1)))  
     model.add(Flatten())  
     model.add(Dense(512))
-    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(LeakyReLU(alpha=0.2))  # Changed here
     model.add(Dense(256))
-    model.add(LeakyReLU(negative_slope=0.2))
+    model.add(LeakyReLU(alpha=0.2))  # Changed here
     model.add(Dense(1, activation='sigmoid'))
     return model
 
 discriminator = build_discriminator()
 discriminator.compile(optimizer=Adam(0.0002, 0.5), loss='binary_crossentropy', metrics=['accuracy'])
 
-discriminator.trainable=False
-gan_input=Input(shape=(100,))
-generated_image=generator(gan_input)
-gan_output=discriminator(generated_image)
-gan=Model(gan_input,gan_output)
-gan.compile(optimizer=Adam(0.0002,0.5),loss='binary_crossentropy')
+discriminator.trainable = False
+gan_input = Input(shape=(100,))
+generated_image = generator(gan_input)
+gan_output = discriminator(generated_image)
+gan = Model(gan_input, gan_output)
+gan.compile(optimizer=Adam(0.0002, 0.5), loss='binary_crossentropy')
 
 def train_gan(epochs, batch_size=128):
     X_train, _ = mnist.load_data()
@@ -82,17 +82,18 @@ def train_gan(epochs, batch_size=128):
             save_images(epoch)
 
 def save_images(epoch):
-    r,c=5,5
-    noise=np.random.normal(0,1,(r*c,100))
-    generated_images=generator.predict(noise)
-    generated_images=0.5*generated_images+0.5
-    fig,axs=plt.subplots(r,c)
-    count=0
+    r, c = 5, 5
+    noise = np.random.normal(0, 1, (r * c, 100))
+    generated_images = generator.predict(noise)
+    generated_images = 0.5 * generated_images + 0.5
+    fig, axs = plt.subplots(r, c)
+    count = 0
     for i in range(r):
         for j in range(c):
-            axs[i,j].imshow(generated_images[count,:,:,0],cmap='gray')
-            axs[i,j].axis('off')
-            count+=1
+            axs[i, j].imshow(generated_images[count, :, :, 0], cmap='gray')
+            axs[i, j].axis('off')
+            count += 1
     fig.savefig(f"gan_images_{epoch}.png")
     plt.close()
-train_gan(epochs=10000,batch_size=64)
+
+train_gan(epochs=10000, batch_size=64)
